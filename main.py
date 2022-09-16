96de080efa864ab7b5554741ce6779c123b89653
Bassam AL SAEDY <basaidi@gmail.com>
2022-09-16 16:32:24 +0200

    Buttons

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import tkinter as tk
 from tkinter import Canvas, filedialog, Text
 import os
+from turtle import pd
 
 root = tk.Tk()
 
-canvas = tk.Canvas(root, height=500, width=500, bg="#263D42")
+# Box size and color
+canvas = tk.Canvas(root, height=400, width=400, bg="#263D42")
 canvas.pack()
 
+# 2nd box size and color
 frame = tk.Frame(root, bg="white")
 frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
+
+# Buttons
+open_file = tk.Button(root, text="Open File", padx=10,
+                      pady=5, fg="white", bg="#263D42")
+
+# Showing the button
+open_file.pack()
+
+run_apps = tk.Button(root, text="Run Apps", padx=10,
+                     pady=5, fg="white", bg="#263D42")
+
+# Showing the button
+run_apps.pack()
 
 root.mainloop()
